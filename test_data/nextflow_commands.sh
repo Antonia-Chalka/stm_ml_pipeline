e6342ad2d3ff0421b7f013a52af0da7edbd745d6
Annita Desktop <s1438773@ed.ac.uk>
2022-01-25 14:43:04 +0000

    update docker container & template commands for blast

--- test_data/nextflow_commands.sh
+++ test_data/nextflow_commands.sh
@@ -3,3 +3,19 @@
 nextflow run pipeline_dsl2.nf --outdir /home/annita/repos/stm_ml_pipeline-1/test_data/model_build_out/ --assemblypath /home/annita/repos/stm_ml_pipeline-1/test_data/model_build_in --hostdata /home/annita/repos/stm_ml_pipeline-1/test_data/model_build_in/metadata.csv --year_collected="Collection.Year" -profile docker -resume
 
 nextflow run pipeline_dsl2.nf -profile docker -resume
+
+nextflow run pipeline_dsl2.nf --outdir /home/annita/repos/initial_data_run/out/ --assemblypath /home/annita/repos/initial_data_run/assemblies --hostdata /home/annita/repos/initial_data_run/all_metadata.csv --year_collected="Collection.Year" -profile docker -resume
+
+
+
+sed '/>/ s/_+.*//g' representative_clusters_merged.fasta > cleaned.fasta
+head -n 1 igr_all.tsv | sed 's/\"//g' | tr -s ' \t' '\n' | head -n -1 > igr.list
+seqtk subseq cleaned.fasta igr.list > igr_filter.fasta
+
+
+combine assemblies and give unique headers
+
+makeblastdb -in all_assemblies.fasta (file-name of the contigs or whatever) -dbtype nucl  -out assemblyblastdb
+
+tblastx -query input (with the gene file) -db assemblyblastdb -out outname (file name with the results)
+
